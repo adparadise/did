a362cb529015b481737024d17af17c45dce6e2c6
adp-drosera <adparadise@gmail.com>
2011-05-22 16:38:59 -0400

    fix to use of autocomplete before initialized

--- lib/did/env.rb
+++ lib/did/env.rb
@@ -3,53 +3,59 @@ require 'pathname'
 require 'sqlite3'
 
 class Env
-  def self.setup_active_record(profile_name, allow_create = true)
-    initialize_home(profile_name, allow_create)
-    config = {"adapter" => 'sqlite3', "database" => database_path(profile_name).to_s}
+  attr_reader :profile_name
+
+  def initialize(profile_name, allow_create = true)
+    @profile_name = profile_name
+    @allow_create = allow_create
+  end
+
+  def allow_create?
+    @allow_create
+  end
+
+  def setup_active_record
+    initialize_home
+    config = {"adapter" => 'sqlite3', "database" => database_path.to_s}
     ActiveRecord::Base.establish_connection(config)
   end
 
-  def self.setup_raw(profile_name, allow_create = true)
-    initialize_home(profile_name, allow_create)
-  end
-
-  def self.setup_development(environment_name)
+  def setup_development(environment_name)
     config = development_database_config(environment_name)
     ActiveRecord::Base.establish_connection(config)
   end
 
-  def self.db(profile_name, allow_create = false)
-    database_path = database_path(profile_name)
-    return nil if !allow_create && !database_path.exist?
+  def database_file_exist?
+    database_path.exist?
+  end
+
+  def db
+    return nil if !allow_create? && !database_file_exist?
     @@db ||= SQLite3::Database.new(database_path.to_s)
   end
 
   private
 
-  def self.initialize_home(profile_name, allow_create = true)
-    database_path = database_path(profile_name)
-    return if !allow_create && !database_path.exist?
-
+  def initialize_home
     did_home.mkpath
     database_path.dirname.mkpath
-    if !database_path.exist?
+    if !database_file_exist?
       puts "initializing DID database: #{database_path}"
-      db = db(profile_name, allow_create)
       File.readlines("db/schema.sql").each do |line|
         db.execute(line)
       end
     end
   end
 
-  def self.did_home
+  def did_home
     (Pathname.new('~').expand_path + ".did").realdirpath
   end
 
-  def self.database_path(profile_name)
+  def database_path
     did_home + profile_name + "did.db"
   end
 
-  def self.development_database_config(environment_name)
+  def development_database_config(environment_name)
     database_config_path = 'db/config.yml'
     raise "database configuration file not found at: #{database_config_path}" unless File.exist?(database_config_path)
     config_all = YAML.load(File.read(database_config_path))
@@ -60,9 +66,11 @@ class Env
   end
 end
 
+DID_PROFILE = ENV['DID_PROFILE'] || 'default'
+$env = Env.new(DID_PROFILE, defined?(ALLOW_CREATE) ? ALLOW_CREATE : true)
+exit if !$env.allow_create? && !$env.database_file_exist?
 unless defined?(ENVIRONMENT_NAME)
-  # production environment
-  Env.setup_active_record(DID_PROFILE)
+  $env.setup_active_record
 else
-  Env.setup_development(ENVIRONMENT_NAME)
+  $env.setup_development(ENVIRONMENT_NAME)
 end
